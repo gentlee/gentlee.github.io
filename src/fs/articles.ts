4c8f034768e18293d277a736777abb9e1ecdae8a
gentlee <dienes_gainful_3p@icloud.com>
2025-12-03 01:37:31 +0300

    small refactoring

--- src/fs/articles.ts
+++ src/fs/articles.ts
@@ -27,37 +27,35 @@ export const getArticleSlugs = async () => {
 export const getArticleHtmlAndFrontmatter = async (slug: string, lang: Language) => {
   const folderName = (await fs.readdir('articles')).find((x) => x.endsWith(slug))! // Contains date and slug.
 
-  return await (async () => {
-    const parsedArticle = await unified()
-      .use(remarkParse) // Parse markdown.
-      .use(remarkGfm) // Enable github flavored markdown (tables).
-      .use(remarkFrontmatter) // Parse frontmatter.
-      .use(remarkExtractFrontmatter, {yaml: yaml.parse, remove: true}) // Extract frontmatter.
-      .use(remarkRehype, {allowDangerousHtml: true}) // Allow raw HTML in markdown (details, summary).
-      .use(rehypeRaw) // Process raw HTML in markdown (details, summary).
-      .use(rehypeStringify) // Serialize HTML.
-      .process(await fs.readFile(`articles/${folderName}/${lang}.md`, 'utf-8'))
+  const parsedArticle = await unified()
+    .use(remarkParse) // Parse markdown.
+    .use(remarkGfm) // Enable github flavored markdown (tables).
+    .use(remarkFrontmatter) // Parse frontmatter.
+    .use(remarkExtractFrontmatter, {yaml: yaml.parse, remove: true}) // Extract frontmatter.
+    .use(remarkRehype, {allowDangerousHtml: true}) // Allow raw HTML in markdown (details, summary).
+    .use(rehypeRaw) // Process raw HTML in markdown (details, summary).
+    .use(rehypeStringify) // Serialize HTML.
+    .process(await fs.readFile(`articles/${folderName}/${lang}.md`, 'utf-8'))
 
-    const frontmatter = parsedArticle.data as {
-      title: string
-      date: string
-      cover: string
-      'cover-alt': string
-      spoiler: string
-      links?: Record<Source, string>[]
-    }
+  const frontmatter = parsedArticle.data as {
+    title: string
+    date: string
+    cover: string
+    'cover-alt': string
+    spoiler: string
+    links?: Record<Source, string>[]
+  }
 
-    return {
-      slug,
-      folderName,
-      frontmatter: {
-        ...frontmatter,
-        titlePlain: removeXml(frontmatter.title),
-        spoilerPlain: removeXml(frontmatter.spoiler),
-      },
-      html: parsedArticle.toString(),
-    }
-  })()
+  return {
+    slug,
+    folderName,
+    frontmatter: {
+      ...frontmatter,
+      titlePlain: removeXml(frontmatter.title),
+      spoilerPlain: removeXml(frontmatter.spoiler),
+    },
+    html: parsedArticle.toString(),
+  }
 }
 
 export const getUrlForArticleImage = (image: string, folder: string) => {
